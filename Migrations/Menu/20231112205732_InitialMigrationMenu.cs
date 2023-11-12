8ffa73a4950af11f19777d57d199a47c0ab89b54
Federico <federeyes2000@gmail.com>
2023-11-12 18:03:48 -0300

    AgregadaBD Menu y arreglado Type

---
+++ Migrations/Menu/20231112205732_InitialMigrationMenu.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clase4.Migrations.Menu
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsVegetarian = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Calories = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menu");
+        }
+    }
+}
